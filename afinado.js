7d67fd8f190b7ab0fa2b57bb5a1799866d78feda
Valentina Kachinovsky <valentinakachi@gmail.com>
2022-06-17 11:55:29 -0300

    Agregando paginas HTML y modificando el DOM para darle la bienvenida al usuario (desafío complementario)

--- afinado.js
+++ afinado.js
@@ -1,13 +1,13 @@
-/*let ingresarNombre = prompt("Ingresa tu nombre!");
+let ingresarNombre = prompt("Ingresa tu nombre!");
 let holaUsuario = alert("Bienvenido/a a Ordenados, " + ingresarNombre + "! ");
-let bienvenida = alert("En este juego deberás ordenar las letras para formar palabras. ¡Adelante!")*/
+let bienvenida = alert("En este juego deberás ordenar las letras para formar palabras. ¡Adelante!")
 
 const letrasMinus = [
   "a","b","c","d","e","f","g","h","i",
   "j","k","l","m","n","ñ","o","p","q",
   "r","s","t","u","v","w","x","y","z"
 ];
-console.log(letrasMinus.length);
+
 const letrasMay = [
   "A","B","C","D","E","F","G","H","I",
   "J","K","L","M","N","Ñ","O","P","Q",
@@ -19,18 +19,27 @@ const vocalesTildes = [
 
 
 class Palabras {
-  constructor(categoria, solucion, pista, nivel) {
+  constructor(categoria, solucion, pista) {
     this.categoria = categoria;
     this.solucion = solucion;
     this.pista = pista;
-    this.nivel = nivel;
 }
   pedirpista() {
-    alert(this.pista); // luego sería un alert, pero por ahora lo dejo así 
+    //alert(this.pista);  
   }
 }
-const avestruz = new Palabras ("animal", "Avestruz", "Alas y cuello largo", 1);
-const ornitorrinco = new Palabras("animal", "Ornitorrinco", "Mamífero de cuello muy muy largo!", 3);
+const avestruz = new Palabras ("animal", "Avestruz", "Alas y cuello largo");
+const ornitorrinco = new Palabras("animal", "Ornitorrinco", "Mamífero de cuello muy muy largo!");
+const murcielago = new Palabras("animal", "Murciélago", "Soy un animal que le gusta dormir con mi cabeza para abajo...");
+
+let ornitorrincoOrdenado = ornitorrinco.solucion;
+let ornitorrincoDesordenado = prompt("Ordena la siguiente palabra: ocnirrotinrO.")
+if (ornitorrincoDesordenado == ornitorrincoOrdenado) {
+  alert("Bien hecho!!")
+} else {
+  alert("Sigue intentando!");
+    
+  }
 
 const paises = [
   {pais: "Uruguay", continente: "Latinoamérica"},
@@ -41,21 +50,15 @@ const paises = [
   {pais: "Cuba", continente: "Centroamérica"}
 ]
 paises.pop();
-console.log(paises);
 paises.reverse();
-console.log(paises);
-
-
-
-console.log(avestruz);
-console.log(ornitorrinco.pista);
+paises.slice(0,2);
 
 let nivelSuperado = true
 let puntos = 0
 function puntaje() {
   if (nivelSuperado) {
     puntos += 1;
-    console.log("Felicitaciones! Ganaste un punto");
+    //alert("Felicitaciones! Ganaste un punto");
   } 
 }
 puntaje();
@@ -67,25 +70,6 @@ while (osoDesordenado != oso) {
 osoDesordenado = prompt("Sigue intentando!")
 }
 alert("Correcto! La palabra es Oso.")
-//CLASE DOM 
-/*console.dir(document)
-const titulo = document.getElementById('titulo');
-console.log(titulo.innerText);
-
-const divs = document.getElementsByClassName('bienvenida') //usar Foreach
-for (div of divs) {
-  console.log(divs);
-}
-
-const etiquetaDiv = document.getElementsByTagName('div')
-console.log(etiquetaDiv[0].innerHTML);
-
-let bienvenida = document.querySelectorAll(".bienvenida");
-console.log(bienvenida);*/
-
-
-
-
 
 let elefante = letrasMay[4] + letrasMinus[11] + letrasMinus[4] + letrasMinus[5] + letrasMinus[0] + letrasMinus[13] + letrasMinus[20] + letrasMinus[4];
 let elefanteDesordenado = prompt("Ordena las siguientes letras: " + letrasMinus[11] + letrasMay[4] + letrasMinus[5] + letrasMinus[13] + letrasMinus[0] + letrasMinus[20] + letrasMinus[4] + letrasMinus[4] + ". Pista: animal. ");
@@ -94,11 +78,10 @@ while (elefanteDesordenado != elefante) {
 }
 alert("Correcto! La palabra es Elefante.")
 //CONSULTO LENGTH PARA PODER VER EL ÍNDICE DE CADA ELEMENTO EN LA CONSOLA //
-letrasMinus.length;
-console.log(letrasMinus);
+
 
 let gato = letrasMay[6] + letrasMinus[0] + letrasMinus[20] + letrasMinus[15];
-console.log(gato);
+
 
 let nandu = letrasMay[14] + letrasMinus[0] + letrasMinus[13] + letrasMinus[3] + vocalesTildes[4];
 let nanduDesordenado = prompt("Ordena las siguientes letras: " + letrasMinus[0] + letrasMay[14] + vocalesTildes[4] + letrasMinus[13] + letrasMinus[3] + ". Pista: animal");
@@ -108,29 +91,6 @@ while (nanduDesordenado != nandu) {
 alert("Correcto! Felicitaciones. Esta si que era difícil!")
 
 const perro = letrasMay[16] + letrasMinus[4] + letrasMinus[18] + letrasMinus[18] + letrasMinus[15];
-console.log(perro);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // EJERCICIO EN CLASE
@@ -156,4 +116,26 @@ console.log(perro);
 //   console.log(elementosFinales);
 //   elementosFinales.reverse();
 //   console.log(elementosFinales);
+
+
+console.dir(document)
+let titulo = document.getElementById('titulo');
+titulo.innerText = titulo.innerHTML + " " + ingresarNombre;
+console.log(titulo.innerText)
+
+
+//CLASE DOM 
+/*const divs = document.getElementsByClassName('bienvenida') //usar Foreach
+for (div of divs) {
+  console.log(divs);
+}
+
+const etiquetaDiv = document.getElementsByTagName('div')
+console.log(etiquetaDiv[0].innerHTML);
+
+let bienvenida = document.querySelectorAll(".bienvenida");
+console.log(bienvenida);*/
+
+
+
 
