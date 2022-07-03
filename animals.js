2b0d7b3306e4e443f65550577a66443187394820
Valentina <valentinakachi@gmail.com>
2022-07-02 22:23:17 -0300

    Dom y dom

--- animals.js
+++ animals.js
@@ -1,16 +1,19 @@
+const animales = [];
 class Palabras {
-    constructor(categoria, solucion, pista) {
-      this.categoria = categoria;
+    constructor(desordenado, solucion, pista) {
+      this.desordenado = desordenado;
       this.solucion = solucion;
       this.pista = pista;
   }
   }
-  const avestruz = new Palabras ("animal", "Avestruz", "Alas y cuello largo");
-  const ornitorrinco = new Palabras("animal", "Ornitorrinco", "Mamífero de cuello muy muy largo!");
-  const murcielago = new Palabras("animal", "Murciélago", "Soy un animal que le gusta dormir con mi cabeza para abajo...");
-  
+  const avestruz = new Palabras ("zArtseuv","Avestruz", "Alas y cuello largo");
+  animales.push(avestruz);
+  const ornitorrinco = new Palabras("rOintrorinco","Ornitorrinco", "Mamífero de cuello muy muy largo!");
+  animales.push(ornitorrinco);
+  const murcielago = new Palabras("lagoMéurci","Murciélago", "Soy un animal que le gusta dormir con mi cabeza para abajo...");
+  animales.push(murcielago);
 
-
+console.log(animales);
   const paises = [
     {pais: 'gyuUaru'},
     {pais: 'iSagnrup'},
@@ -24,19 +27,27 @@ class Palabras {
     let paisito = document.createElement('h1');
     let inputPais = document.createElement('input');
     let subPais = document.createElement('button');
-    paisito.innerText= "ORDENA LAS SIGUIENTES LETRAS: " + (pais.pais);
+    paisito.innerText= (pais.pais);
     document.body.append(paisito);
     document.body.append(inputPais);
     document.body.append(subPais);
-    console.log(pais);
+    paisito.className = "bienvenida--mod";
+    subPais.className = "bienvenida";
+    subPais.innerText = "¡Ordenar!";
+    subPais.style.margin = "7px";
+    subPais.style.display = "flex"
+    subPais.style.justifyContent = "center";
+    
+
+    inputPais.className = "entradaLetra";
   };
 
 const miString = JSON.stringify(avestruz);
 const guardarAvestruz = localStorage.setItem('avestruz', miString);
 
+ m
 
-
-const ordenelo = document.querySelector('#ordene');
+let ordenelo = document.querySelector('#ordene');
 const formAnimal = document.querySelector('#formAnimales');
 const ornito = document.querySelector('#orni');
 const ornitoSub = document.querySelector('#subAnimal');
@@ -56,3 +67,4 @@ let incorrecto = document.createElement("p");
     document.body.append(incorrecto);
   }
 });
+
