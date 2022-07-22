fe1633abd000f24959e740e5eb2652f49a5d6f2b
Valentina <valentinakachi@gmail.com>
2022-07-21 23:36:52 -0300

    Avances

--- paises.js
+++ paises.js
@@ -1,19 +1,4 @@
-const animales = [];
-class Palabras {
-    constructor(desordenado, solucion, pista) {
-      this.desordenado = desordenado;
-      this.solucion = solucion;
-      this.pista = pista;
-  }
-  }
-  const avestruz = new Palabras ("zArtseuv","Avestruz", "Alas y cuello largo");
-  animales.push(avestruz);
-  const ornitorrinco = new Palabras("rOintrorinco", "Ornitorrinco", "Mamífero de cuello muy muy largo!");
-  animales.push(ornitorrinco);
-  const murcielago = new Palabras("lagoMéurci","Murciélago", "Soy un animal que le gusta dormir con mi cabeza para abajo...");
-  animales.push(murcielago);
 
-console.log(animales);
   const paises = [
     {pais: 'gyuUaru', organizado: 'Uruguay'},
     {pais: 'iSagnrup', organizado: 'Singapur'},
@@ -29,7 +14,6 @@ console.log(animales);
   let solucionIngresada;
 
     for (const pais of paises) {
-    let ordenados = document.createElement('form');
     let paisito = document.createElement('h1');
     let inputPais = document.createElement('input');
     let subPais = document.createElement('button');
@@ -87,6 +71,6 @@ fetch('data.json')
 
                 
 
-                lista.append(a)
-    
+                lista.append(a);
+
   });
